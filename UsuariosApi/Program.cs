fd14f111870a8b9335fbd36eef4e89b7f5ea96bd
Marcos Paulo <marcos.poo@gmail.com>
2024-02-15 16:48:40 -0300

    Aula 4 - Controle de acesso
    Nessa aula, aprendemos:
    - Tokens JWT são utilizados para trafegar dados de maneira prática entre aplicações;
    - Como adicionar autorização com o método AddAuthorization();
    - Como adicionar autenticação com o método AddAuthentication();
    - Políticas de acesso podem ser usadas para restringir o acesso de usuários;
    - Podemos interceptar requisições a autorizá-las com um AuthorizationHandler.

--- UsuariosApi/Program.cs
+++ UsuariosApi/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using UsuariosApi.Authorization;
 using UsuariosApi.Data;
 using UsuariosApi.Models;
 using UsuariosApi.Services;
@@ -24,11 +29,35 @@ builder.Services
 builder.Services.AddAutoMapper
     (AppDomain.CurrentDomain.GetAssemblies());
 
+builder.Services.AddSingleton<IAuthorizationHandler, IdadeAuthorization>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme =
+        JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MP3000UIUUQHAYMHACIKQUCKAQ98303")),
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("IdadeMinima", policy => 
+        policy.AddRequirements(new IdadeMinima(18))
+    );
+});
 
 builder.Services.AddScoped<UsuarioService>();
 builder.Services.AddScoped<TokenService>();
@@ -43,6 +72,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
